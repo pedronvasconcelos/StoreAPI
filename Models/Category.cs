d6049ed2082949e36f896a0a3c8f664a29ec0b0a
pedrovasconcelos <pedrohnvasconcelos@gmail.com>
2022-03-28 22:38:26 -0300

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace StoreAPI.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? ImageUrl { get; set; }
+    
+}
---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace StoreAPI.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string? Description { get; set; }
+
+    public float Stock { get; set; }
+
+    public DateTime RegisterDate { get; set; }
+    
+
+}
